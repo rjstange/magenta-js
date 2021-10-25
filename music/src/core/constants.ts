87278918bcc32d214847097bfb85d4057eb53dc9
Riley Stange <rjstrange@gmail.com>
2021-10-25 13:55:45 -0500

    Changed DEFAULT_DRUM_PITCH_CLASSES to fit into the way Toontrack maps their midi drum files

--- music/src/core/constants.ts
+++ music/src/core/constants.ts
@@ -36,26 +36,65 @@ export const DEFAULT_TICKS_PER_QUARTER = 220;
 export const DEFAULT_CHANNEL = 0;
 export const DRUM_CHANNEL = 9;
 export const NON_DRUM_CHANNELS : ReadonlyArray<number> =
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15];
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 14, 15];
+
+// Modded by Riley Stange for Toontrack Midi Inference
 export const DEFAULT_DRUM_PITCH_CLASSES: number[][] = [
-  // bass drum
-  [36, 35],
-  // snare drum
-  [38, 27, 28, 31, 32, 33, 34, 37, 39, 40, 56, 65, 66, 75, 85],
-  // closed hi-hat
-  [42, 44, 54, 68, 69, 70, 71, 73, 78, 80],
-  // open hi-hat
-  [46, 67, 72, 74, 79, 81],
-  // low tom
-  [45, 29, 41, 61, 64, 84],
-  // mid tom
-  [48, 47, 60, 63, 77, 86, 87],
-  // high tom
-  [50, 30, 43, 62, 76, 83],
-  // crash cymbal
-  [49, 55, 57, 58],
-  // ride cymbal
-  [51, 52, 53, 59, 82]
+
+    [12], // Hi-hat: Open Tip 0
+    [13], // Hi-hat: Open Tip 1
+    [17], // Hi-hat: Open Tip 5
+    [21, 44], // Hi-hat: Closed Pedal
+    [22], // Hi-hat: Closed Edge
+    [23], // Hi-hat: Open Pedal
+    [24], // Hi-hat: Open Edge 1
+    [25, 46], // Hi-hat: Open Edge 2
+    [26], // Hi-hat: Open Edge 3
+    [27, 89], // Cymbal 2: Bow Tip
+    [28, 90], // Cymbal 2: Bell Shank
+    [29, 116], // Ride: Bow Shank
+    [30, 117], // Ride: Bell Tip
+    [31, 101], // Cymbal 4: Bow Tip
+    [32, 102], // Cymbal 4: Bell Shank
+    [34, 36], // Kick: Open
+    [35], // Kick: Hit
+    [37], // Snare: Sidestick
+    [38], // Snare: Center
+    [39], // Snare: Closed Roll
+    [40], // Snare: Rimshot 
+    [41], // Floortom 2: Center
+    [43], // Floortom 1: Center 
+    [45], // Racktom 3: Center 
+    [47], // Racktom 2: Center 
+    [48], // Racktom 1: Center 
+    [49, 91], // Cymbal 2: Crash
+    [50, 94], // Cymbal 2: Mute Hit 
+    [51, 113], // Ride: Bow Tip
+    [52, 110], // Cymbal 5: Crash
+    [53, 114], // Ride: Bell Shank
+    [54, 107], // Cymbal 5: Mute Hit 
+    [55, 98], // Cymbal 3: Crash
+    [57, 103], // Cymbal 4: Crash
+    [59, 115], // Ride: Edge
+    [60], // Hi-hat: Open Edge 4
+    [61, 11, 42], // Hi-hat: Closed Tip
+    [62], // Hi-hat: Tight Edge
+    [63], // Hi-hat: Tight Tip
+    [67], // Snare: Forward Swirl
+    [68], // Snare: Muted
+    [69], // Snare: Flam 
+    [70], // Snare: Muted Accent 
+    [73], // Floortom 2: Rimshot 
+    [75], // Floortom 1: Rimshot
+    [78], // Racktom 3: Rimshot 
+    [80], // Racktom 2: Rimshot 
+    [82], // Racktom 1: Rimshot 
+    [84], // Cymbal 1: Bow Tip 
+    [85], // Cymbal 1: Bell Shank 
+    [86], // Cymbal 1: Crash 
+    [88], // Cymbal 1: Bell Tip 
+    [126], // Snare: Backward Brush 
+    [127] // Snare: Forward Brush
 ];
 
 // Velocity-related constants.
